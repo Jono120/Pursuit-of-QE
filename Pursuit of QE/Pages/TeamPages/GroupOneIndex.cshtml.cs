c35683c6ab5efa0f33f39d96a011b4807b4533c7
Jonathan <Jonathan.lake@mbie.govt.nz>
2022-11-23 19:49:08 +1300

    updating models

--- Pursuit of QE/Pages/TeamPages/GroupOneIndex.cshtml.cs
+++ Pursuit of QE/Pages/TeamPages/GroupOneIndex.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace Pursuit_of_QE.Pages.TeamPages
 {
