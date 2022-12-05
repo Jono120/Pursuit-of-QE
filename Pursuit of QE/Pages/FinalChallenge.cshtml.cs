f9f87c9dfd739fafc7b16a95156fa235b4cabfd7
Jonathan <Jonathan.lake@mbie.govt.nz>
2022-12-06 12:24:15 +1300

    adding in new pages and setting challenge titles

---
+++ Pursuit of QE/Pages/FinalChallenge.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pursuit_of_QE.Pages
+{
+    public class FinalChallengeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
